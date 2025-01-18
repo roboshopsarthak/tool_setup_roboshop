ed72d8bb715c76bf547b7516f3f9dd01a6eb1d44
Sarthak1207 <sarthak.sharma1207@gmail.com>
2025-01-18 06:11:44 +0530

    Making infra changes

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -9,6 +9,13 @@ terraform {
 
 variable "vault_token" {}
 
+provider "vault" {
+    address = "http://vault-internal.sarthak1207.shop:8200"
+    token = var.vault_token
+    skip_tls_verify = true
+}
+
+
 
 resource "vault_mount" "roboshop-dev" {
   path        = "roboshop-dev"

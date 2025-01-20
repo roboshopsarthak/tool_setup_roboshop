632c85513e6299d9499c7e3fb406be3d912e9dc6
Sarthak1207 <sarthak.sharma1207@gmail.com>
2025-01-20 13:33:56 +0530

    tool setup github-runner

--- module-ec2/iam.tf
+++ module-ec2/iam.tf
@@ -17,18 +17,32 @@ resource "aws_iam_role" "role" {
     tags = {
         name = "${var.tool_name}_role"
     }
-    inline_policy {
-    name = "${var.tool_name}_inline_role"
+}
 
-    policy = <<EOF
-        Version = "2012-10-17"
-        Statement = [
-        {
-            Action   = var.policy_list
-            Effect   = "Allow"
-            Resource = "*"
-        EOF
+resource "aws_iam_policy" "policy" {
+  name        = "${var.tool_name}_inline_role"
+  description = "Policy for ${var.tool_name} role"
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": ${jsonencode(var.policy_list)},
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+  tags = {
+        name = "${var.tool_name}_role_copilot"
     }
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
